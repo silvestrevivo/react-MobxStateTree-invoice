2efc15aa6817cf56d965cc7adc7e4c0aedcf856f
silvestrevivo <silvestrevivomillan@gmail.com>
2018-07-17 21:48:12 +0200

    adding mobx devtools and onpatch to debug the project

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom'
 import '../assets/sass/style.scss'
 import App from './components/app'
 
+import {onPatch} from 'mobx-state-tree'
+import makeInspectable from 'mobx-devtools-mst'
+
 import Invoice from './models/invoice'
 
 const invoice = Invoice.create({ currency: 'CAD' })
+
+onPatch(invoice, patch => {
+  console.log(patch)
+})
+makeInspectable(invoice)
 
 ReactDOM.render(<App invoice={invoice} />, document.getElementById('root'))
